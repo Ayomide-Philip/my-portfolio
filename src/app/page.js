d6c8f756ea55f6607b82d60fdc086be02447baf6
Areo Ayomide <areoayomide2008@gmail.com>
2025-06-18 23:27:21 +0100

    feat: add scroll indicator with animated progress using framer-motion

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,26 @@
+"use client";
+import { motion, useScroll } from "framer-motion";
 import AboutSection from "./componet/home/aboutMe";
 import HeroSection from "./componet/home/hero";
 
 export default function Home() {
+  const { scrollYProgress } = useScroll();
   return (
     <>
+      <motion.div
+        id="scroll-indicator"
+        style={{
+          scaleX: scrollYProgress,
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+          height: 10,
+          originX: 0,
+          backgroundColor: "orange",
+        }}
+      />
+
       <HeroSection />
       <AboutSection />
     </>
