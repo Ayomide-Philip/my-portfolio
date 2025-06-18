3da36fb7dc13e650ecd58c19f67fb1d20b0cde1d
Areo Ayomide <areoayomide2008@gmail.com>
2025-06-18 21:08:16 +0100

    remove unused button group from Home component layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,31 +1,6 @@
 export default function Home() {
   return (
     <div className="fixed bottom-0 z-50 w-full -translate-x-1/2 bg-white border-t border-gray-200 left-1/2 dark:bg-gray-700 dark:border-gray-600">
-      <div className="w-full">
-        <div
-          className="grid max-w-xs grid-cols-3 gap-1 p-1 mx-auto my-2 bg-gray-100 rounded-lg dark:bg-gray-600"
-          role="group"
-        >
-          <button
-            type="button"
-            className="px-5 py-1.5 text-xs font-medium text-gray-900 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-700 rounded-lg"
-          >
-            New
-          </button>
-          <button
-            type="button"
-            className="px-5 py-1.5 text-xs font-medium text-white bg-gray-900 dark:bg-gray-300 dark:text-gray-900 rounded-lg"
-          >
-            Popular
-          </button>
-          <button
-            type="button"
-            className="px-5 py-1.5 text-xs font-medium text-gray-900 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-700 rounded-lg"
-          >
-            Following
-          </button>
-        </div>
-      </div>
       <div className="grid h-full max-w-lg grid-cols-5 mx-auto">
         <button
           data-tooltip-target="tooltip-home"
