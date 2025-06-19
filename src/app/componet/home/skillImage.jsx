ce19b9efe595cd3fe447db2dd1eadf502cfa8080
Areo Ayomide <areoayomide2008@gmail.com>
2025-06-19 22:45:43 +0100

    refactor: update icon imports and enhance iconDetails array

--- src/app/componet/home/skillImage.jsx
+++ src/app/componet/home/skillImage.jsx
@@ -2,26 +2,23 @@ import {
   FaHtml5,
   FaCss3Alt,
   FaJs,
-  FaPhp,
+  FaBootstrap,
   FaGitAlt,
-  FaLaravel,
 } from "react-icons/fa";
-import { FaReact } from "react-icons/fa6";
-import { RiJavascriptFill } from "react-icons/ri";
-import {
-  SiTypescript,
-  SiNuxtdotjs,
-  SiVuedotjs,
-  SiTailwindcss,
-  SiFigma,
-} from "react-icons/si";
+import { FaReact, FaNode } from "react-icons/fa6";
+import { RiNextjsFill } from "react-icons/ri";
+import { SiTailwindcss } from "react-icons/si";
 
 const iconDetails = [
   { iconCode: FaHtml5, color: "#E34F26", label: "HTML5" },
   { iconCode: FaCss3Alt, color: "#1572B6", label: "CSS3" },
   { iconCode: FaJs, color: "#F7DF1E", label: "JavaScript" },
+  { iconCode: FaBootstrap, color: "#563D7C", label: "Bootstrap " },
   { iconCode: SiTailwindcss, color: "#2196f3", label: "Tailwindcss" },
+  { iconCode: FaGitAlt, color: "#F1502F", label: "Git" },
   { iconCode: FaReact, color: "#61DAFB", label: "React JS" },
+  { iconCode: FaNode, color: "#8CC84B", label: "Node JS" },
+  { iconCode: RiNextjsFill, color: "#FFFFFF", label: "Nextjs" },
 ];
 export default function SkillImage() {
   return (
